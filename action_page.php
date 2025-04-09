62c9cde7d69a0d2d8e3c2d7be113f0b2a124ef68
hhh <166711707+oc4tche@users.noreply.github.com>
2025-04-09 14:27:26 +0200

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,12 +1,9 @@
 <?php
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // Retrieve the username and password from the form
       $username = $_POST["username"];
       $password = $_POST["password"];
       
-      // Validate the user's credentials
       if($username == "user" && $password == "pass") {
-         // Redirect the user to their dashboard page
          header("Location: action_php.php");
          exit;
       } else {
